d1f0261be9f9ddb4bc501431de8ccb12778cf8b2
Rich Jenks <rich@richjenks.com>
2014-04-06 16:33:46 +0100

    Moved template files into folder

---
+++ template/header.php
@@ -0,0 +1,12 @@
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title><?=$dir->get_request();?></title>
+		<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+		<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
+		<link rel="stylesheet" href="<?php echo TEEPEE;?>/style.css">
+	</head>
+	<body>
+		<div class="wrapper">
