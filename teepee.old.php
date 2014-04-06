52e62e42948d0a8bd9bab93662c25a1e661625a3
Rich Jenks <rich@richjenks.com>
2014-04-06 18:04:10 +0100

    Not sure what changed as this file is only for reference

--- teepee.old.php
+++ teepee.old.php
@@ -285,7 +285,7 @@ $summary .= '</section>';
 		<title><?=$dir['path'];?></title>
 		<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
 		<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
 		<link rel="stylesheet" href="<?php echo $dir['teepee'];?>/style.css">
 	</head>
