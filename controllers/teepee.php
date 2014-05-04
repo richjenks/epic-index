ad17231dfc0b8a893ea60fe6d2299b0ebe599a41
Rich Jenks <rich@richjenks.com>
2014-05-04 11:30:37 +0100

    Began MVC

---
+++ controllers/teepee.php
@@ -0,0 +1,14 @@
+<?php
+
+require '../models/dir.php';
+require '../models/file.php';
+require '../models/helper.php';
+
+class Teepee {
+
+	// Get current dir
+	// Create dir object for dir
+	// Create dir object for parent & subdirs
+	// Create file object for files
+
+}
--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
 
-// Path to Teepee from webroot
-define('TEEPEE', '/resources/teepee/');
+// Path to Teepee
+define('TEEPEE', '/home/rich/Dropbox/Dev/php/resources/teepee/');
 
-// Required files
-require 'includes/helper.class.php';
-require 'includes/dir.class.php';
-require 'includes/file.class.php';
-
-$dir = new Dir;
-
-require 'template/header.php';
-echo $dir->breadcrumbs();
-echo $dir->list_children();
-echo $dir->summary();
-require 'template/footer.php';
+// Start
+require 'controller/teepee.php';
+$teepee = new Teepee;
