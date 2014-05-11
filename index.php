da7ec9cdd7ce96b32df4d8c41baba439e58572c7
Rich Jenks <rich@richjenks.com>
2014-05-11 14:45:03 +0100

    Improved load order

--- index.php
+++ index.php
@@ -5,12 +5,8 @@ namespace RichJenks\Teepee;
 // Path to Teepee
 define('TEEPEE_PATH', __DIR__.'/');
 
-// Requires
+// Helper class
 require TEEPEE_PATH.'app/Helper.php';
-require TEEPEE_PATH.'app/models/File.php';
-require TEEPEE_PATH.'app/models/Dir.php';
-require TEEPEE_PATH.'app/controllers/Controller.php';
-require TEEPEE_PATH.'app/controllers/Teepee.php';
 
 // URI to Teepee
 define('TEEPEE_URI', dirname(Helper::file_uri(__FILE__)).'/');
@@ -19,6 +15,12 @@ define('TEEPEE_URI', dirname(Helper::file_uri(__FILE__)).'/');
 if (TEEPEE_URI.'index.php' === Helper::get_uri()) {
 	die ('<script>console.log("Yo dawg I herd you like to list files so I listed the files in the file that lists files so you can list files in the file list.");</script>');
 }
+
+// Requires
+require TEEPEE_PATH.'app/models/File.php';
+require TEEPEE_PATH.'app/models/Dir.php';
+require TEEPEE_PATH.'app/controllers/Controller.php';
+require TEEPEE_PATH.'app/controllers/Teepee.php';
 
 // Configuration options
 global $config;
