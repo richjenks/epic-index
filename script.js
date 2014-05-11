7243d5a690bc5223a2c7e0f856ab7429be82088f
Rich Jenks <rich@richjenks.com>
2014-05-11 13:12:11 +0100

    Updated styles

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Load readme content
 $.ajax({
-	url: "https://rawgit.com/richjenks/teepee/master/readme.md",
+	url: "https://rawgit.com/richjenks/teepee/dev/readme.md",
 	dataType: 'text',
 	success: function(data) {
 		
 		// Convert readme from markdown to html
 		var converter = new Markdown.Converter();
+
+		// Show html
 		$(".wrapper").html(converter.makeHtml(data));
 	
 	}
