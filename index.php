9c94742598249de62008e84c8143c339979f1d43
Rich Jenks <rich@richjenks.com>
2014-06-08 16:53:21 +0100

    Formatted required - still no point autoloading because every require is needed for every execution

--- index.php
+++ index.php
@@ -5,8 +5,20 @@ namespace RichJenks\Teepee;
 // Path to Teepee
 define('TEEPEE_PATH', __DIR__.'/');
 
-// Main Helper
+// Helpers
 require TEEPEE_PATH.'app/helpers/Helper.php';
+require TEEPEE_PATH.'app/helpers/Translator.php';
+require TEEPEE_PATH.'app/helpers/Config.php';
+
+// Controllers
+require TEEPEE_PATH.'app/controllers/Controller.php';
+require TEEPEE_PATH.'app/controllers/Auth.php';
+require TEEPEE_PATH.'app/controllers/Teepee.php';
+
+// Models
+require TEEPEE_PATH.'app/models/Links.php';
+require TEEPEE_PATH.'app/models/Dir.php';
+require TEEPEE_PATH.'app/models/File.php';
 
 // URI to Teepee
 define('TEEPEE_URI', dirname(Helper::file_uri(__FILE__)).'/');
@@ -16,15 +28,6 @@ if (TEEPEE_URI.'index.php' === Helper::get_uri()) {
 	die ('Yo dawg I herd you like to list files so I listed the files in the file that lists files so you can list files in the file list.');
 }
 
-// Requires - no point autoloading as all are needed for every execution
-require TEEPEE_PATH.'app/controllers/Controller.php';
-require TEEPEE_PATH.'app/controllers/Auth.php';
-require TEEPEE_PATH.'app/controllers/Teepee.php';
-require TEEPEE_PATH.'app/models/Links.php';
-require TEEPEE_PATH.'app/models/Dir.php';
-require TEEPEE_PATH.'app/models/File.php';
-require TEEPEE_PATH.'app/helpers/Translator.php';
-require TEEPEE_PATH.'app/helpers/Config.php';
 
 // Configuration options
 global $config;
